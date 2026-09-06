c05b8b45ceb96b6200998ce3215c98ec824a6c4e
Mustafa ALKAN <3alkan@gmail.com>
2026-09-06 19:26:24 +0300

    fix: update auto_run_threshold values for scrape_courses and scrape_programs jobs

--- src/robotdegilim_xyz_backend/worker/registry.py
+++ src/robotdegilim_xyz_backend/worker/registry.py
@@ -8,11 +8,11 @@ from robotdegilim_xyz_backend.jobs.scrape_programs.run import run_scrape_program
 JOB_REGISTRY = {
     "scrape_courses": {
         "handler": run_scrape_courses,
-        "auto_run_threshold": timedelta(hours=12)
+        "auto_run_threshold": timedelta(hours=2)
     },
     "scrape_programs": {
         "handler": run_scrape_programs,
-        "auto_run_threshold": timedelta(hours=24)
+        "auto_run_threshold": None
     },
     # Future jobs can simply be added here!
 }
